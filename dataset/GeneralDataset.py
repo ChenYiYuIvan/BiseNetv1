50124d11218d00fc3d965ec0ebd629a740dba45a
ChenYiYuIvan <chenivan998@gmail.com>
2022-05-11 22:21:09 +0200

    removed unused function

--- dataset/GeneralDataset.py
+++ dataset/GeneralDataset.py
@@ -50,7 +50,7 @@ class GeneralDataset(Dataset):
         image = Image.open(image_path)
         label = Image.open(label_path)
 
-        if not self.data_augmentation:  # need to resize because there is no crop into correct size
+        if not self.data_augmentation or self.mode == 'val':  # need to resize when there is no crop into correct size
             resize = transforms.Resize(self.img_size)
             image = resize(image)
             label = resize(label)
@@ -70,26 +70,14 @@ class GeneralDataset(Dataset):
         label = self.label_mapping[label]
         label = torch.from_numpy(label)
 
-        if self.data_augmentation and self.mode == 'train':  # apply random horizontal flip and random scale
-            seed = random.random()
-            flip_bool, new_size = self.get_aug_params(prob=0.5, seed=seed)
+        if self.data_augmentation and self.mode == 'train':  # apply random horizontal flip
+            flip_bool = random.random() < 0.5
 
             if flip_bool:
                 image = torch.flip(image, [-1])
                 label = torch.flip(label, [-1])
 
         return image, label.squeeze()
-
-    def get_aug_params(self, prob, seed):
-        random.seed(seed)
-
-        flip_bool = random.random() < prob
-
-        scales = [0.75, 1.0, 1.5, 1.75, 2.0]
-        scale = random.choice(scales)
-        new_size = (int(self.img_size[0] * scale), int(self.img_size[1] * scale))
-
-        return flip_bool, new_size
 
     def random_scale(self, tensor, interpolation, seed):
         random.seed(seed)
